22b35133b233c7b0b6108c15ab570381e31d194d
Eileen Ai <eileen.aix@gmail.com>
2018-10-26 15:48:28 -0400

    DONE

--- src/conditions_loops/labs/Exercise_05.java
+++ src/conditions_loops/labs/Exercise_05.java
@@ -1,5 +1,8 @@
 package conditions_loops.labs;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 5: Calculator
  *
@@ -16,4 +19,27 @@ package conditions_loops.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
+
+        //create scanner
+        Scanner scanner = new Scanner(System.in);
+        //prompt user
+        System.out.println("Enter first number: ");
+        //assign input1 to variable as int1
+        float number1 = scanner.nextInt();
+        //create scanner
+        Scanner scanner2 = new Scanner(System.in);
+        //prompt user
+        System.out.println("Enter second number: ");
+        //assign input1 to variable as int1
+        float number2 = scanner2.nextInt();
+
+        //calculation
+        float sum = 0;
+        for(float i = number1; i <= number2; i++){
+            sum +=i;
+        }
+        System.out.println("The sum is: " + sum);
+        System.out.println("The average is: " + sum/ (number2 - number1 +1));
+    }
 }
