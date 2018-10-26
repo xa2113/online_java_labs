a99ff82704f5235216b1b8e54b7d09779e40373b
Eileen Ai <eileen.aix@gmail.com>
2018-10-25 21:46:08 -0400

    DONE

--- src/conditions_loops/labs/Exercise_01.java
+++ src/conditions_loops/labs/Exercise_01.java
@@ -24,6 +24,10 @@ public class Exercise_01 {
         int number = scanner.nextInt();
 
         // write completed code here
-
+        if(number%2==0){
+            System.out.println("Even!");
+        }else{
+            System.out.println("Odd!");
+        }
     }
 }
