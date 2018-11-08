de8db33eaa14f57ffc41433a7cd17fda5118de4c
Eileen Ai <eileen.aix@gmail.com>
2018-11-07 20:19:09 -0500

    DONE

--- src/objects_classes_methods/labs/Exercise_04.java
+++ src/objects_classes_methods/labs/Exercise_04.java
@@ -8,3 +8,32 @@ package objects_classes_methods.labs;
  */
 
 
+public class Exercise_04 {
+
+    //method one - takes in two ints and returns the average as an int
+    int average(int a, int b){
+        return (a + b)/2;
+    }
+
+    //method two - takes in three ints and returns the average as an int
+    int average(int a, int b, int c){
+        return (a + b + c)/3;
+    }
+
+    //method two - takes in four doubles and returns the average as an int
+    int average(double a, double b, double c, double d){
+        return (int)((a + b + c + d)/4);
+    }
+
+}
+
+class Example{
+
+    //question: how to return result of a method?? i.e. What to do to return result instead of psvm.
+    public static void main(String[] args) {
+
+        Exercise_04 obj = new Exercise_04();
+        int result = obj.average(1,1,1);
+        System.out.println(result);
+    }
+}
