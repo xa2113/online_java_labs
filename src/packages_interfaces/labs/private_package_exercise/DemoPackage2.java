c8c094f9b3bc39364c2f93da1c949a4f94475c41
Eileen Ai <eileen.aix@gmail.com>
2018-11-19 18:50:24 -0500

    commit

--- src/packages_interfaces/labs/private_package_exercise/DemoPackage2.java
+++ src/packages_interfaces/labs/private_package_exercise/DemoPackage2.java
@@ -1,10 +1,20 @@
-package packages_interfaces.labs.private_package_exercise.;
+package packages_interfaces.labs.private_package_exercise;
+
+import packages_interfaces.labs.package_exercise.DemoPackage;
 
 public class DemoPackage2 {
 
     public static void main(String[] args) {
         DemoPackage demo = new DemoPackage();
+        demo.num = 10;
 
+        Example e = new Example();
     }
 
+}
+
+class Example extends DemoPackage{
+    void setProtectedVal(){
+        super.protectedNum = 12;
+    }
 }
