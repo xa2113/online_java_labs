aef7321a6823a78da15c2eb13aeab37d17f508db
Eileen Ai <eileen.aix@gmail.com>
2018-11-19 19:36:03 -0500

    DONE

--- src/exception_handling/labs/Exercise_03.java
+++ src/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,24 @@ package exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+class Exercise_03 {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 0;
+
+        try{
+            int result = a/b;
+            System.out.println(result);
+        }
+        catch(ArithmeticException exc){
+            System.out.println("This is an arithmetic exception!");
+        }
+        finally{
+            System.out.println("Print regardless of whether there is an exception or not.");
+        }
+    }
+
+}
