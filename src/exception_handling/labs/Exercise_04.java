bad404d6c763e7ecd5723747fdfe7dd13eb61edf
Eileen Ai <eileen.aix@gmail.com>
2018-11-19 19:36:26 -0500

    question

--- src/exception_handling/labs/Exercise_04.java
+++ src/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,24 @@ package exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+
+class Exercise_04 {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 0;
+
+        try{
+            System.out.println(a/b);
+        }
+        catch(IndexOutOfBoundsException exc){
+            System.out.println("Index out of bounds!");
+            try {
+                System.out.println(a/b);
+            } catch(ArithmeticException exc2){
+                System.out.println("Arithmetic exception.");
+            }
+        }
+    }
+}
