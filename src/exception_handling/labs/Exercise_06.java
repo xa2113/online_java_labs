27c4ab3fea70966503d02eca7df2d44c5c6ffb49
Eileen Ai <eileen.aix@gmail.com>
2018-12-04 21:20:22 +0800

    done

--- src/exception_handling/labs/Exercise_06.java
+++ src/exception_handling/labs/Exercise_06.java
@@ -7,3 +7,43 @@ package exception_handling.labs;
  *
  */
 
+class Exercise_06 {
+
+    public static void oneException() {
+
+        int numer[] = {1, 2, 4, 5, 6, 7, 8, 9};
+        int denom[] = {2, 0, 0, 5};
+
+
+        for (int i = 0; i < numer.length; i++) {
+            try {
+                System.out.println(numer[i] + " / " +
+                        denom[i] + " is " +
+                        numer[i] / denom[i]);
+
+            } catch (ArithmeticException exc) {
+                System.out.println("Can't divide by Zero.");
+            } catch (ArrayIndexOutOfBoundsException exc) {
+                throw exc;
+            }
+        }
+
+    }
+
+}
+
+class ThrowExercise {
+
+    public static void main(String[] args) {
+
+        try {
+            Exercise_06.oneException();
+        }
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("No matching element");
+        }
+    }
+
+ }
+
+
