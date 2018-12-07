77802c668f4d6f5fec39fce315ca915093ee1a05
Eileen Ai <eileen.aix@gmail.com>
2018-12-07 14:48:31 +0800

    done

--- src/exception_handling/labs/Exercise_05.java
+++ src/exception_handling/labs/Exercise_05.java
@@ -1,5 +1,7 @@
 package exception_handling.labs;
 
+import java.lang.reflect.Array;
+
 /**
  * Exception Handling Exercise 5:
  *
@@ -9,6 +11,23 @@ package exception_handling.labs;
 
 class Exercise_05 {
 
+    public static void main(String[] args) {
 
+        int numer[] = {1,2,4,5,6,7,7};
+        int denom[] = {2,0,3,0,0};
 
+        for(int i = 0; i < numer.length; i++) {
+            try{
+                System.out.println(numer[i] + " / " +
+                        denom[i] + " is " +
+                        numer[i] / denom[i]);
+
+            } catch (ArrayIndexOutOfBoundsException ai){
+                System.out.println("No matching element found.");
+            }
+            catch (ArithmeticException ae){
+                throw ae;
+            }
+        }
+     }
 }
