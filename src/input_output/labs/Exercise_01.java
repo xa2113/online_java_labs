311cec6bbb1f33080939fc67d5a8d3a10b8918d3
Eileen Ai <eileen.aix@gmail.com>
2018-12-09 22:22:36 +0800

    WIP

--- src/input_output/labs/Exercise_01.java
+++ src/input_output/labs/Exercise_01.java
@@ -1,5 +1,9 @@
 package input_output.labs;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -8,4 +12,39 @@ package input_output.labs;
  *
  *
  */
+
+//class Exercise_01 {
+//
+//    public static void main(String[] args) throws IOException {
+//
+//        int i;
+//        BufferedInputStream fin = null;
+//        BufferedOutputStream fout = null;
+//
+//        try {
+//            fin = new BufferedInputStream(args[0]);
+//
+//            fout = new BufferedOutputStream(args[0]);
+//
+//            do {
+//                i = fin.read();
+//                if (i != -1) fout.write(i);
+//            } while (i != -1);
+//        } catch(IOException exc) {
+//            System.out.println("I/0 Error: " + exc);
+//        } finally {
+//
+//            try{
+//                if(fin != null) fin.close();
+//            } catch(IOException exc) {
+//                System.out.println("Error Closing Input File");
+//            }
+//            try{
+//                if(fout != null) fout.close();
+//            } catch(IOException exc) {
+//                System.out.println("Error Closing Output File");
+//            }
+//        }
+//    }
+//}
 
