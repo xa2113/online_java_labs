22450c9493e8106715f2df77cd4eeb0b552fdd48
Eileen Ai <eileen.aix@gmail.com>
2018-12-09 22:43:31 +0800

    WIP

--- src/input_output/labs/Exercise_02.java
+++ src/input_output/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File encryption
  *
@@ -12,3 +14,21 @@ package input_output.labs;
  *
  */
 
+class Exercise_02 {
+
+    public static void main(String[] args) {
+
+        String s;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/Users/Eileen/Documents/CodingNomads/online_course/test2.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("test_output.txt"));
+
+            while((s=br.readLine()) !=null){
+                System.out.println(s);
+            }
+        } catch(IOException exc){
+            System.out.println("I/O Error: " + exc);
+        }
+    }
+}
