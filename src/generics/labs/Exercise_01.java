08a6553e9820c9471092c42f7b874b2eae9449f3
Eileen Ai <eileen.aix@gmail.com>
2018-12-14 16:04:44 +0800

    WIP

--- src/generics/labs/Exercise_01.java
+++ src/generics/labs/Exercise_01.java
@@ -8,3 +8,42 @@ package generics.labs;
  *          dynamic usage.
  */
 
+class Exercise_01<T> {
+
+//    T variable1;
+//    T variable2;
+
+    T ob;
+
+    Exercise_01(T o){ob = o;}
+
+    T getob(){ return ob;}
+
+    void showType(){
+        System.out.println("Type of T is " +
+                ob.getClass().getName());
+    }
+}
+
+class GenericsInterger {
+    public static void main(String[] args) {
+
+        //Integer example
+        Exercise_01<Integer> intOb;
+
+        intOb = new Exercise_01<>(3);
+
+//        int v = intOb.getob();
+        System.out.println(intOb.getob());
+        intOb.showType();
+
+        //String example
+        Exercise_01<String> variable1;
+
+        variable1 = new Exercise_01<>("random");
+
+        System.out.println(variable1.getob());
+        variable1.showType();
+
+    }
+}
