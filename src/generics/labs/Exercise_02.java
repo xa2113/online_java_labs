0b2131bd00164211629d1445c0382ae9f419f911
Eileen Ai <eileen.aix@gmail.com>
2019-01-31 13:48:46 +0800

    finished

--- src/generics/labs/Exercise_02.java
+++ src/generics/labs/Exercise_02.java
@@ -6,18 +6,18 @@ package generics.labs;
  *      Create a class with a generic method that takes in an array and prints out the value at a certain index.
  */
 
-//class Exercise_02<T>{
-//
-//    public static void someArray(T[] x){
-//        System.out.println(x[2]);
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        Integer nums[] = { 1, 2, 3, 4, 5 };
-//        String strings[] = { "a","b","c","d" };
-//
-//        someArray(nums[]);
-//    }
-//
-//}
+class Exercise_02<T>{
+
+    public static void main(String[] args) {
+        Integer nums[] = { 1, 2, 3, 4, 5 };
+        String strings[] = { "a","b","c","d" };
+
+        MyGenericArray<Integer> genericArray = new MyGenericArray<>();
+
+        genericArray.someArray(nums);
+
+        MyGenericArray<String> genericArray2 = new MyGenericArray<>();
+    }
+
+}
+
---
+++ src/generics/labs/MyGenericArray.java
@@ -0,0 +1,11 @@
+package generics.labs;
+
+public class MyGenericArray<T> {
+
+    public void someArray(T[] x){
+
+        System.out.println(x[2]);
+
+    }
+
+}
